9f1fd357e407f03cc6d37deb983c840718cb54fa
Success Jimoh-daodu <successjimoh-daodu@Jimohs-MacBook-Pro.local>
2019-10-12 18:26:41 +0100

    server change

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get("/", (req,res) => {
-    res.json(database.users)
-})
+// app.get("/", (req,res) => {
+//     res.json(database.users)
+// })
 
 app.post("/signin", (req,res) => { signin.handleSignIn(req,res,db,bcrypt) })
 
